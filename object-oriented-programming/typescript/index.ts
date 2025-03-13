bef7e5498129640e9d21079ce1207518b5dc2960
Josep Najera <jjnajera@outlook.es>
2025-03-12 19:27:57 -0600

    feat(class): add folder structure

--- object-oriented-programming/typescript/index.ts
+++ object-oriented-programming/typescript/index.ts
@@ -1,3 +1,49 @@
-import {getUserName} from './prompt'
+type DogColor = 'black' | 'white'
 
-const name = getUserName()
+interface Address {
+  city: string
+  state: string
+}
+
+class Person {
+  constructor(public name: string, public address: Address) {
+    this.name = name
+    this.address = address
+  }
+}
+class BabyDog {
+  private age: number
+
+  constructor(private name: string = '', public color: DogColor = 'black', public owner: Person) {
+    this.name = name
+    this.color = color
+    this.age = 0
+    this.owner = owner
+  }
+
+  public setAge = (age: number): void => {
+    this.age++
+  }
+
+  public getAge = (): number => {
+    return this.age
+  }
+
+  private cry = (): void {
+    console.log('</3')
+  }
+
+  public bark = (): void => {
+    console.log('GUAU GUAU!')
+    this.cry()
+  }
+
+}
+
+const p1 = new Person('Josep', { city: 'Santa Ana', state: 'SJ' })
+
+const bd1 = new BabyDog('Oreo', 'black', p1)
+bd1.bark()
+bd1.owner.name
+const bd2 = new BabyDog('Puppy', 'white', p1)
+bd2.bark
